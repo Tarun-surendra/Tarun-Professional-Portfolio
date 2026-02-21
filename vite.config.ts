7d54e3b30ef42dae78c41920d629bd18a5e748f7
Tarun Surendra <tarunsurendramothukuru@gmail.com>
2026-02-20 22:50:01 -0500

    Configure Vite for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: './',
-  plugins: [react()],
+ base: "/Tarun-Professional-Portfolio/",
+ plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
